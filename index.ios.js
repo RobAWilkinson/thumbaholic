9eb3c7c181e931f200453237d63fd498e4c79b1a
Mariano Echegoyen <mariano.echegoyen@dng.com>
2016-06-11 16:39:59 -0700

    front end v1

--- index.ios.js
+++ index.ios.js
@@ -4,53 +4,202 @@
  * @flow
  */
 
+import FitImage from 'react-native-fit-image'
 import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import { AppRegistry, StyleSheet, Text, Image, View, Navigator, TouchableHighlight } from 'react-native'
 import MapView from 'react-native-maps'
 
-class thumbaholic extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { latitude: 39.78825,  longitude: -128.4324 }
+class Main extends React.Component{
+  navFirstAid() {
+    this.props.navigator.push({
+      id: 'first_aid_page'
+    })
   }
-  componentDidMount = () => {
-    // send data as stringified json with { channel: 'channel', user_id: user, lat: lng }
-    var ws = new WebSocket('ws://thumbaholic.herokuapp.com/');
-    ws.onopen= () => {
-      navigator.geolocation.getCurrentPosition( position => {
-        let { latitude, longitude } = position.coords
-        ws.send(JSON.stringify({latitude, longitude }))
-      });
-    }
-    ws.onmessage = (e) => {
-      // a message was received
-      console.log(e);
-      navigator.geolocation.getCurrentPosition( position => {
-        let { latitude, longitude } = position.coords
-        console.log(latitude);
-        console.log(longitude);
-        ws.send(JSON.stringify({latitude, longitude }))
-      });
-    };
+  navFood() {
+    this.props.navigator.push({
+      id: 'food_page'
+    })
   }
-  render () {
-    console.log('render');
-    console.log(this.state);
+  navRestrooms() {
+    this.props.navigator.push({
+      id: 'restrooms_page'
+    })
+  }
+
+  render() {
     return (
-    <View style={styles.map}>
-      <MapView style={styles.map} initialRegion={{  latitude: this.state.latitude,  longitude: this.state.longitude,  latitudeDelta: 0.0922,  longitudeDelta: 0.0421}} />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.logo_container}>
+          <FitImage 
+            style={styles.logo}
+            source={{uri: 'http://defiant-nation.surge.sh/powered_by_thumbaholics.png'}}/> 
+
+          </View>
+        
+
+        <TouchableHighlight onPress={this.navFirstAid.bind(this)}>
+          <View>
+            <FitImage
+              style={styles.fird_aid}
+              source={{uri: 'http://pink-vessel.surge.sh/first_aid.jpg'}}/>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={this.navFood.bind(this)}>
+          <View>
+            <FitImage
+              style={styles.food}
+              source={{uri: 'http://materialistic-sugar.surge.sh/food.jpg'}}/>
+          </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={this.navRestrooms.bind(this)}>
+          <View>
+            <FitImage
+              style={styles.restrooms}
+              source={{uri: 'http://materialistic-sugar.surge.sh/restrooms.jpg'}}/>
+          </View>
+        </TouchableHighlight>
+      </View>
     )
   }
 }
+
+class FirstAid extends React.Component{
+  navMain() {
+    this.props.navigator.pop({
+      id: 'first_aid_page'
+    })
+      console.log("clicked again!")
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.second_page}>
+          First Aid
+        </Text>
+
+        <TouchableHighlight onPress={this.navMain.bind(this)}>
+          <Text style={styles.button}>
+             Go back to page one!
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+class Food extends React.Component{
+  navMain() {
+    this.props.navigator.pop({
+      id: 'food_page'
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.second_page}>
+          Food & Drinks
+        </Text>
+
+        <TouchableHighlight onPress={this.navMain.bind(this)}>
+          <Text style={styles.button}>
+             Go back to page one!
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+class Restrooms extends React.Component{
+  navMain() {
+    this.props.navigator.pop({
+      id: 'restrooms_page'
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.second_page}>
+          Restrooms
+        </Text>
+
+        <TouchableHighlight onPress={this.navMain.bind(this)}>
+          <Text style={styles.button}>
+             Go back to page one!
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+class thumbaholic extends React.Component{
+  render() {
+    return (
+      <Navigator
+        style={styles.container}
+        initialRoute={{id: 'main'}}
+        renderScene={this.navigatorRenderScene}/>
+    );
+  }
+
+  navigatorRenderScene(route, navigator) {
+    _navigator = navigator;
+    switch (route.id) {
+      case 'main':
+        return (<Main navigator={navigator} data={route.data} title="main"/>);
+      case 'first_aid_page':
+        console.log("second")
+        return (<FirstAid navigator={navigator} title="first_aid_page" />);
+      case 'food_page':
+        console.log("food")
+        return (<Food navigator={navigator} title="food_page" />);
+      case 'restrooms_page':
+        console.log("restrooms")
+        return (<Restrooms navigator={navigator} title="restrooms_page" />);
+    }
+  }
+}
+
 const styles = StyleSheet.create({
-  map: {
+  container: {
+    backgroundColor: 'white',
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0
+    bottom: 0,
+    paddingTop: 20
+  },
+  first_page: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  second_page: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+    textAlign: 'center',
+    padding: 10,
+  },
+  logo: {
+    width: 300,
+  },
+  logo_container: {
+    alignItems: 'center',
+    padding: 10
+  },
+  goto_firstaid: {
+
   }
+
 })
 
 AppRegistry.registerComponent('thumbaholic', () => thumbaholic)
