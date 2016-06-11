c8fa5f1bb06035c1dc793d52fea8e6b8a9002dec
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2016-06-10 20:50:25 -0700

    gets coords

--- index.ios.js
+++ index.ios.js
@@ -9,10 +9,23 @@ import { AppRegistry, StyleSheet, Text, View } from 'react-native'
 import MapView from 'react-native-maps'
 
 class thumbaholic extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { latitude: 39.78825,  longitude: -128.4324 }
+  }
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition( position => {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+      this.setState({latitude: position.coords.latitude, longitude: position.coords.longitude })
+    });
+  }
   render () {
+    console.log('render');
+    console.log(this.state);
     return (
     <View style={styles.map}>
-      <MapView style={styles.map} initialRegion={{  latitude: 37.78825,  longitude: -122.4324,  latitudeDelta: 0.0922,  longitudeDelta: 0.0421}} />
+      <MapView style={styles.map} initialRegion={{  latitude: this.state.latitude,  longitude: this.state.longitude,  latitudeDelta: 0.0922,  longitudeDelta: 0.0421}} />
     </View>
     )
   }
